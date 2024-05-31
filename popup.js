2361ea80c3f79f59959d7d0b1d0e6445cbb79678
kaisirius <garvitkhurana47@gmail.com>
2024-05-31 14:47:55 +0530

    color pallete fix

--- popup.js
+++ popup.js
@@ -2,23 +2,28 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   let PenStatus=false;
   let HighlighterStatus=false;
+  let ColorStatus = "#FFFF00";
+  // chrome.storage.local.set({ColorStatus});
   // loading saved tools state
-  chrome.storage.local.get(['PenStatus', 'HighlighterStatus'], (result) => {
-    PenStatus = result.PenStatus || false;
-    HighlighterStatus = result.HighlighterStatus || false;
-    UpdateButton();
-  });
-  
   function UpdateButton(){
     document.getElementById('pen').style.backgroundColor = PenStatus ? '#D4D4D4' : 'FFFFFF';
     document.getElementById('highlighter').style.backgroundColor = HighlighterStatus ? '#D4D4D4' : 'FFFFFF';
+    document.getElementById('stroke-color').value = ColorStatus.toString();
   }
+
+  chrome.storage.local.get(['PenStatus', 'HighlighterStatus','ColorStatus'], (result) => {
+    PenStatus = result.PenStatus || false;
+    HighlighterStatus = result.HighlighterStatus || false;
+    ColorStatus = result.ColorStatus || '#FFFF00';
+    UpdateButton();
+  });
   
   //css will change on click
   document.getElementById('pen').addEventListener('click', () => {
     PenStatus=!PenStatus;
     HighlighterStatus=false;
-    chrome.storage.local.set({ PenStatus, HighlighterStatus });
+    console.log(PenStatus);
+    chrome.storage.local.set({ PenStatus, HighlighterStatus, ColorStatus});
     UpdateButton();
 
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
@@ -34,7 +39,7 @@ document.addEventListener('DOMContentLoaded',()=>{
   document.getElementById('highlighter').addEventListener('click', () => {
     HighlighterStatus=!HighlighterStatus;
     PenStatus=false;
-    chrome.storage.local.set({ PenStatus, HighlighterStatus });
+    chrome.storage.local.set({ PenStatus, HighlighterStatus, ColorStatus });
     UpdateButton();
 
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
@@ -49,6 +54,10 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   document.getElementById('stroke-color').addEventListener('change', (event) => {
     const selectedColor=event.target.value;
+    ColorStatus = selectedColor;
+    chrome.storage.local.set({ PenStatus, HighlighterStatus, ColorStatus });
+    UpdateButton();
+
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
       if (tabs.length > 0) {
         console.log("Sending 'color' action to content script");
@@ -81,5 +90,6 @@ document.addEventListener('DOMContentLoaded',()=>{
     });
   });
 
+  
 });
   
